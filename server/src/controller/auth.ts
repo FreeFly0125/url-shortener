c57ac33d34c484ba4c43e739a88dd335f17eee52
FreeFly0125 <135254388+FreeFly0125@users.noreply.github.com>
2025-01-23 13:25:17 +0100

    auth flow in server

---
+++ server/src/controller/auth.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { AuthService } from "service";
+
+interface AuthData {
+  username: string;
+  password: string;
+}
+
+export const signIn = async (req: Request, res: Response) => {
+  const data: AuthData = req.body;
+
+  const result = await AuthService.getUser(data.username, data.password);
+  if (!result.username) return res.status(404).send("User not found!");
+  else if (!result.password) return res.status(401).send("Incorrect password!");
+  else return res.status(200).send({ username: result.username });
+};
+
+export const signUp = async (req: Request, res: Response) => {
+  const data: AuthData = req.body;
+  const result = await AuthService.newUser(data.username, data.password);
+  return result
+    ? res.status(200).send({ success: true })
+    : res.status(500).send({ success: false });
+};
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import urlRouter from "./urlRoute";
+import authRouter from "./authRoute";
 
 const appRouter = Router();
 
 appRouter.use("/url", urlRouter);
+appRouter.use("/auth", authRouter);
 
 export default appRouter;
