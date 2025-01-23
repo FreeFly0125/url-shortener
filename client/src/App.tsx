c317cdb49c07c90d054fa191122ebe4d710399e3
FreeFly0125 <135254388+FreeFly0125@users.noreply.github.com>
2025-01-23 13:58:02 +0100

    user auth in client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Dashboard } from "./pages";
+import { Dashboard, UserAuth } from "./pages";
+import { PATH } from "./consts";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={PATH.INTERFACE} element={<Navigate to={PATH.SIGNIN} />} />
+        <Route path={PATH.DASHBOARD} element={<Dashboard />} />
+        <Route path={PATH.SIGNIN} element={<UserAuth isSignIn={true} />} />
+        <Route path={PATH.SIGNUP} element={<UserAuth isSignIn={false} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
